5503ebc84fa675792b11f4658ae3233e56b74462
valdezanton97 <valdezanton97@gmail.com>
2025-10-03 14:36:49 +0800

    Fixed web3forms

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,61 +1,49 @@
 <template>
-  <!-- contact -->
-  <h1 class="text-center my-4 pt-5" id="contact">Contact</h1>
-  <div class="contact-section">
-    <div class="row align-items-center mt-4">
-      <div class="col-md-6 map-container">
-        <iframe
-          id="gmap_canvas"
-          src="https://maps.google.com/maps?q=centro%20escolar%20university%20manila&t=&z=13&ie=UTF8&iwloc=&output=embed"
-          frameborder="0"
-          scrolling="no"
-          marginheight="0"
-          marginwidth="0"
-        ></iframe>
-      </div>
+  <div class="container" id="contact">
+    <h2>Contact me!</h2>
+    <div class="row my-5 justify-content-center">
       <div class="col-md-6">
         <form @submit.prevent="submitForm">
-          <div class="mb-3">
-            <input
-              type="text"
-              v-model="name"
-              class="form-control contact-form-control"
-              placeholder="First Name M.I. Last Name"
-              required
-            />
-          </div>
-          <div class="mb-3">
-            <input
-              type="email"
-              v-model="email"
-              class="form-control contact-form-control"
-              placeholder="Email"
-              required
-            />
-          </div>
-          <div class="mb-3">
-            <textarea
-              v-model="message"
-              class="form-control contact-form-control"
-              rows="6"
-              placeholder="Message"
-              required
-            ></textarea>
-          </div>
-          <div class="form-footer">
-            <div class="social-icons">
-              <!-- your socials -->
-              <a href="https://www.linkedin.com/in/charles-babbage-8291a6211/" id="linkedin"><i class="fab fa-linkedin"></i></a>
-              <a href="https://gitlab.com/cbabbage0991" id="gitlab"><i class="fab fa-gitlab"></i></a>
-              <a href="https://github.com/cbabbage0991" id="github"><i class="fab fa-github"></i></a>
-            </div>
-            <button
-              type="submit"
-              class="submit-btn pl-5 pr-5"
-              :disabled="isLoading"
-            >
-              {{ isLoading ? "Sending..." : "Submit" }}
-            </button>
+          <input
+            type="text"
+            name="name"
+            v-model="form.name"
+            placeholder="First Name, Last Name"
+            class="form-control"
+            required
+            autocomplete
+          />
+          <input
+            type="email"
+            name="email"
+            v-model="form.email"
+            placeholder="Email"
+            class="form-control mt-1"
+            required
+            autocomplete
+          />
+          <textarea
+            rows="5"
+            class="form-control mt-1"
+            placeholder="Enter your message here..."
+            v-model="form.message"
+            required
+          ></textarea>
+
+          <div class="d-flex justify-content-center gap-2 mt-3">
+            <a href="https://www.linkedin.com/" target="_blank">
+              <img src="https://cdn-icons-png.flaticon.com/256/174/174857.png" />
+            </a>
+            <a href="https://github.com" target="_blank">
+              <img src="https://cdn-icons-png.flaticon.com/256/25/25231.png" />
+            </a>
+            <a href="https://about.gitlab.com" target="_blank">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTMU0Ge8750e3MFRuLtBi_nu8MlsLnuCT5UQ&s"
+              />
+            </a>
+            <button type="submit" class="form-button ms-auto">Submit</button>
+            <button type="reset" class="form-button">Reset</button>
           </div>
         </form>
       </div>
@@ -63,58 +51,51 @@
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
+<script>
 import { Notyf } from "notyf";
 import "notyf/notyf.min.css";
 
-const notyf = new Notyf();
-
-// Web3Forms Access Key
-const WEB3FORMS_ACCESS_KEY = "96caf4ea-9932-4f87-ba02-62281ddfb7ea";
-const subject = "New message from Portfolio Contact Form";
-
-const name = ref("");
-const email = ref("");
-const message = ref("");
-
-const isLoading = ref(false);
-
-const submitForm = async () => {
-  isLoading.value = true;
-  try {
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
+export default {
+  name: "Contact",
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        message: "",
       },
-      body: JSON.stringify({
-        access_key: WEB3FORMS_ACCESS_KEY,
-        subject: subject,
-        from_name: name.value,
-        from_email: email.value,
-        message: message.value,
-      }),
-    });
+      notyf: new Notyf(),
+    };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({
+            access_key: "YOUR_ACCESS_KEY_HERE",
+            ...this.form,
+          }),
+        });
 
-    const result = await response.json();
+        const result = await response.json();
 
-    if (result.success) {
-      console.log(result);
-      notyf.success("Message Sent!");
-      // Clear inputs
-      name.value = "";
-      email.value = "";
-      message.value = "";
-    } else {
-      notyf.error("Something went wrong.");
-    }
-  } catch (error) {
-    console.error(error);
-    notyf.error("Failed to send message");
-  } finally {
-    isLoading.value = false;
-  }
+        if (result.success) {
+          this.notyf.success("Message sent successfully!");
+          this.form.name = "";
+          this.form.email = "";
+          this.form.message = "";
+        } else {
+          this.notyf.error("Something went wrong. Please try again.");
+        }
+      } catch (error) {
+        this.notyf.error("Error sending message. Please try again.");
+      }
+    },
+  },
 };
 </script>
